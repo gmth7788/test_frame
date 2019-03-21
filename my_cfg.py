e09effeb2c11d76144ac98351651253b6ca2db0b
gmth7788 <orchard2046@163.com>
2019-03-21 10:50:16 +0800

    自省配置文件，自动添加类的属性

--- my_cfg.py
+++ my_cfg.py
@@ -4,22 +4,24 @@
 import os
 import configparser
 
-CONFIG_FILE = r"./sjs_test.ini"
-
 class my_config:
     '''
     获取配置
     '''
 
-    def __init__(self):
-        if os.path.exists(CONFIG_FILE):
-            self.get_config(CONFIG_FILE)
+    def __init__(self, cfg_file=r"./sjs_test.ini"):
+        '''
+        读取配置文件
+        :param cfg_file:
+        '''
+        if os.path.exists(cfg_file):
+            self.get_config(cfg_file)
         else:
-            print("读取配置文件[%s]失败。" % CONFIG_FILE)
+            print("读取配置文件[%s]失败。" % cfg_file)
 
     def get_config(self, cfg_file):
         '''
-        读取配置文件
+        自省配置文件，自动添加类的属性
         :param cfg_file: 配置文件名
         :return:
         '''
@@ -27,9 +29,20 @@ class my_config:
         config.read(cfg_file)
         sections = config.sections()
 
-        self.userid = config['USER']['userid']
-        self.passwd = config['USER']['passwd']
+        for sec in sections:
+            opts = config.options(sec)
+            for opt in opts:
+                setattr(self, opt, config[sec][opt])
 
-        self.log_file = config['COMMON']['log_file']
 
+        #
+        #
+        # self.root_path = config['COMMON']['root_path']
+        # self.log_file = config['COMMON']['log_file']
+        # self.tmp_image_file = config['COMMON']['tmp_image_file']
+        # self.jym_image_file = config['COMMON']['jym_image_file']
+        #
+        # self.baidu_app_id = config['BAIDU_APP']['APP_ID']
+        # self.baidu_api_key = config['BAIDU_APP']['API_KEY']
+        # self.baidu_secret_key = config['BAIDU_APP']['SECRET_KEY']
 
